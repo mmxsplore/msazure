0a4c9795d943275eb6ba84532079d6755a01ce6f
mmxsplore <mmxsplore@gmail.com>
2019-11-08 08:31:14 +0000

    added changes for violet

--- Experiments/WebApplication1/Controllers/HomeController.cs
+++ Experiments/WebApplication1/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApplication1.Controllers
             }
 
             ViewBag.computername = computername;
-            ViewBag.blue_or_green = "BLUE";
+            ViewBag.blue_or_green = "VIOLET";
             ViewBag.utc_time = DateTime.UtcNow;
 
             return View();          
